6aaeabe0ae008ccf99f5e0d765d5679d8eac8f08
Deepak20233 <127834609+Deepak20233@users.noreply.github.com>
2023-04-24 18:44:13 +0530

    Added:Gambling program 2

---
+++ GamblingUC3.java
@@ -0,0 +1,27 @@
+package com.java;
+
+public class GamblingUC3 {
+	public static int dailyStake = 100;
+	public static int betInDollar = 1;
+	public static final int win = 1;
+
+	public static void main(String[] args) {
+		System.out.println("************Welcome To Gambling Simulator***************");
+		
+		while (dailyStake > 50 && dailyStake != 150 ) {
+			int result = (int) ((Math.random() * 10) % 2);
+			if(result == win) {
+				dailyStake++;
+				System.out.println("Won");
+			}else {
+				dailyStake--;
+				System.out.println("Loose");
+			}
+		}
+
+		System.out.println("Total daily Stake is : " + dailyStake);
+        System.out.println("Betting Dollar for every game : " + betInDollar);
+
+	}
+
+}
